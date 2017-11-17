0e03a43bb09d65efc46a588147c8d43d8b4c5bd2
Andreas Neiser <andreas.neiser@qaware.de>
2017-11-17 10:28:04 +0100

    Add two services (one primary) and one controller using the service

--- src/main/java/com/example/mockbeanandprimary/MockbeanAndPrimaryApplication.java
+++ src/main/java/com/example/mockbeanandprimary/MockbeanAndPrimaryApplication.java
@@ -1,12 +1,40 @@
 package com.example.mockbeanandprimary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class MockbeanAndPrimaryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MockbeanAndPrimaryApplication.class, args);
 	}
+}
+
+@Controller
+class SomeController {
+    private final SomeService someService;
+
+    @Autowired
+    SomeController(SomeService someService) {
+        this.someService = someService;
+    }
+}
+
+interface SomeService {
+
+}
+
+@Service
+@Primary
+class SomeServiceImpl implements SomeService {
+
+}
+
+@Service
+class SomeOtherServiceImpl implements SomeService {
+
 }
