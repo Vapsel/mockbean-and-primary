234b82a456166d640e8756b14d3ed92f6f536dd1
Andreas Neiser <andreas.neiser@qaware.de>
2017-11-17 10:21:51 +0100

    Add start.spring.io empty zip

---
+++ src/main/java/com/example/mockbeanandprimary/MockbeanAndPrimaryApplication.java
@@ -0,0 +1,12 @@
+package com.example.mockbeanandprimary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockbeanAndPrimaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockbeanAndPrimaryApplication.class, args);
+	}
+}
